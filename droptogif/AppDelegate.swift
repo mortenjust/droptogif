f6081efc0ccb7ea90a5378d4f7f5396bf78f7a7d
mortenjust <morten.just@genstart.dk>
2015-10-06 22:52:30 -0700

    Initial Commit

---
+++ droptogif/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  droptogif
+//
+//  Created by Morten Just Petersen on 10/6/15.
+//  Copyright (c) 2015 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ droptogif/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  droptogif
+//
+//  Created by Morten Just Petersen on 10/6/15.
+//  Copyright (c) 2015 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
