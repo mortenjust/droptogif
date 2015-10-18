fb4f0793936131ef4988d448de8c8a187a208e98
mortenjust <morten.just@genstart.dk>
2015-10-18 11:31:48 -0700

    add auto size code that failed, so will be deleted in next commit. Dammit.

--- droptogif/Images.xcassets/MovieConverter.swift
+++ droptogif/Images.xcassets/MovieConverter.swift
@@ -31,21 +31,53 @@ class MovieConverter: ShellTaskDelegate {
     }
     
     
-    func convertFile(filename:String, maxSize:Int = -1){
+    func convertFile(filename:String, maxSize:UInt64 = 0){
         print("convertfile for \(filename) with size \(maxSize)")
         
-        if maxSize == -1 {
+        if maxSize == 0 {
             let p = Preferences();
             let fps = Int(p.getFpsPref()!)
             let posterize = Int(p.getPosterizePref()!)
             let scale = Int(p.getScalePercentagePref()!)
             executeConversion(filename, fps: fps!, posterize: posterize, scale: scale)
         } else {
-            // call a function that loops over maxsize
+            optimizeForSize(filename, maxSize: maxSize)
         }
     }
+    
+    func optimizeForSize(filename:String, maxSize:UInt64){
+//        var thisSize:UInt64 = 0;
+//        var lastSize:UInt64 = 0;
+//        
+//        print("let's see wha tthe worst thinkable settings will get us")
+//        var fps:Int = 30
+//        var posterize:Int = 50
+//        var scale:Int = 10
+//        
+//        // recursive function that calls this function until we hit a good size
+//        tryConversionForSize(filename, fps: <#T##Int#>, posterize: <#T##Int#>, scale: <#T##Int#>, completionWithSize: <#T##(UInt64) -> Void#>)
+//        
+    }
+    
+    func tryConversionForSize(filename:String, fps:Int, posterize:Int, scale:Int, completionWithSize: (UInt64) -> Void){
+        let r:Float = Float(scale)/100 // 0.55
+        let scaleArg:String = "-vf scale=iw*\(r):-1"
+        let posterizeArg:String = "-posterize \(posterize)"
+        let fpsArg:String = "\(fps)"
+        
+        let args = [filename, fpsArg, scaleArg, posterizeArg];
+        let gifShellTasker = ShellTasker(scriptFile: "gifify")
+        
+        gifShellTasker.run(arguments: args, complete: { (output) -> Void in
+            let gifFile = "\(filename).gif"; // TODO: This resulting file name gets generated twice. Here, and in gifify.sh
+            let gifSize = Util.use.getFileSize(gifFile)
+            
+            completionWithSize(gifSize)
+            self.taskRunning = false
+        })
+    }
 
-    func executeConversion(filename: String, fps:Int, posterize:Int, scale:Int){
+    func executeConversion(filename: String, fps:Int, posterize:Int, scale:Int) {
         // maybe make a fork in the road here. If maxsize!=null, call normal converter, else call size converter
 
         let r:Float = Float(scale)/100 // 0.55
@@ -68,9 +100,8 @@ class MovieConverter: ShellTaskDelegate {
                 Util.use.openAndSelectFile(gifFile)
             }
             
-            // TODO: Maybe Also return the nwe file's size so that the coming convertFilesToMaxSize function will
-            // be able to determine if it wants to try another pass at a smaller size
             self.delegate.movieConverterDidFinish(gifFile)
+            
         })
 
     }
